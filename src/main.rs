f5f0f34d4b321cfa76ed5c32a8c944cde6850930
SharmaDevanshu089 <SharmaDevanshu089@gmail.com>
2025-08-19 23:29:27 +0530

    Final Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//Takes Basic Input
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Enter a Number");
+    io::stdin().read_line(&mut input).expect("There has been a error");
+    println!("You have entered {}" , input);
 }
